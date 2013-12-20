ea3a39dcf3352dca299dff7f53d6aa33489be03c
Mathias Bynens <mathias@qiwi.be>
2013-12-20 17:08:33 +0100

    Add some tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var assertEquals = assert.equal;
+var assertDeepEquals = assert.deepEqual;
 var assertThrows = assert['throws'];
 
 require('../array-of.js');
@@ -7,4 +8,16 @@ require('../array-of.js');
 assertEquals(Array.of.length, 0);
 assertEquals(Array.propertyIsEnumerable('of'), false);
 
-// TODO
+assertDeepEquals(Array.of(undefined), [undefined]);
+assertDeepEquals(Array.of(null), [null]);
+assertDeepEquals(Array.of(false), [false]);
+assertDeepEquals(Array.of(-Infinity), [-Infinity]);
+assertDeepEquals(Array.of(-0), [-0]);
+assertDeepEquals(Array.of(+0), [+0]);
+assertDeepEquals(Array.of(1), [1]);
+assertDeepEquals(Array.of(1, 2, 3), [1, 2, 3]);
+assertDeepEquals(Array.of(+Infinity), [+Infinity]);
+assertDeepEquals(Array.of({ '0': 'a', '1': 'b', '2': 'c', 'length': 3 }), [{ '0': 'a', '1': 'b', '2': 'c', 'length': 3 }]);
+assertDeepEquals(Array.of(undefined, null, false, -Infinity, -0, +0, 1, 2, +Infinity), [undefined, null, false, -Infinity, -0, +0, 1, 2, +Infinity]);
+
+// TODO: call, apply
