b0edbae874a8b44e88ba10940b89160abc7bd8da
Mathias Bynens <mathias@qiwi.be>
2013-12-22 19:43:29 +0100

    Avoid executing inherited setters in tests
    Thanks to @anba for the suggestion.
    
    Ref. https://github.com/mathiasbynens/Array.of/commit/bbeef75fc657615876ee39ada80f2af3b64d0adb#commitcomment-4925721.

--- tests/tests.js
+++ tests/tests.js
@@ -8,6 +8,7 @@ require('../array-of.js');
 assertEquals(Array.of.length, 0);
 assertEquals(Array.propertyIsEnumerable('of'), false);
 
+assertDeepEquals(Array.of('abc'), ['abc']);
 assertDeepEquals(Array.of(undefined), [undefined]);
 assertDeepEquals(Array.of(null), [null]);
 assertDeepEquals(Array.of(false), [false]);
@@ -20,6 +21,7 @@ assertDeepEquals(Array.of(+Infinity), [+Infinity]);
 assertDeepEquals(Array.of({ '0': 'a', '1': 'b', '2': 'c', 'length': 3 }), [{ '0': 'a', '1': 'b', '2': 'c', 'length': 3 }]);
 assertDeepEquals(Array.of(undefined, null, false, -Infinity, -0, +0, 1, 2, +Infinity), [undefined, null, false, -Infinity, -0, +0, 1, 2, +Infinity]);
 
+assertDeepEquals(Array.of.call(null, 'abc'), ['abc']);
 assertDeepEquals(Array.of.call(null, undefined), [undefined]);
 assertDeepEquals(Array.of.call(null, null), [null]);
 assertDeepEquals(Array.of.call(null, false), [false]);
@@ -37,6 +39,7 @@ assertDeepEquals(Array.of.call(Object, 1, 2, 3), testObject);
 assertEquals(Array.of.call(Object).length, 0);
 assertThrows(function() { Array.of.call(function() { return Object.freeze({}); }); }, TypeError);
 
+assertDeepEquals(Array.of.apply(null, ['abc']), ['abc']);
 assertDeepEquals(Array.of.apply(null, [undefined]), [undefined]);
 assertDeepEquals(Array.of.apply(null, [null]), [null]);
 assertDeepEquals(Array.of.apply(null, [false]), [false]);
@@ -55,11 +58,10 @@ assertEquals(Array.of.apply(Object).length, 0);
 assertThrows(function() { Array.of.apply(function() { return Object.freeze({}); }); }, TypeError);
 
 // Ensure no setters are called for the indexes
-Object.defineProperty(Array.prototype, '0', {
+var MyType = function() {};
+Object.defineProperty(MyType.prototype, '0', {
 	'set': function(x) {
 		throw Error('Setter called: ' + x);
 	}
 });
-assertDeepEquals(Array.of('abc'), ['abc']);
-assertDeepEquals(Array.of.call(null, 'abc'), ['abc']);
-assertDeepEquals(Array.of.apply(null, ['abc']), ['abc']);
+assertDeepEquals(Array.of.call(MyType, 'abc'), { '0': 'abc', 'length': 1 });
