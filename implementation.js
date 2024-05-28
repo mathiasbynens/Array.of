a82ccc8cb7645e2bc88a89b12d849a0073eff550
Jordan Harband <ljharb@gmail.com>
2024-05-28 11:28:31 -0700

    [Refactor] use ES2024 AO implementations

--- implementation.js
+++ implementation.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var ArrayCreate = require('es-abstract/2023/ArrayCreate');
-// var Construct = require('es-abstract/2023/Construct');
-var CreateDataPropertyOrThrow = require('es-abstract/2023/CreateDataPropertyOrThrow');
-var IsConstructor = require('es-abstract/2023/IsConstructor');
-var Set = require('es-abstract/2023/Set');
-var ToString = require('es-abstract/2023/ToString');
+var ArrayCreate = require('es-abstract/2024/ArrayCreate');
+// var Construct = require('es-abstract/2024/Construct');
+var CreateDataPropertyOrThrow = require('es-abstract/2024/CreateDataPropertyOrThrow');
+var IsConstructor = require('es-abstract/2024/IsConstructor');
+var Set = require('es-abstract/2024/Set');
+var ToString = require('es-abstract/2024/ToString');
 
 module.exports = function of() {
 	var len = arguments.length;
